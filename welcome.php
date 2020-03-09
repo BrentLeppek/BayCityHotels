5a1bc9611ec265a29ef1cedbb2609ade1fdb6c4c
BrentLeppek <31483897+BrentLeppek@users.noreply.github.com>
2020-03-09 11:43:52 -0400

    Started the code
    Created welcome page

---
+++ welcome.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
+</head>
+
+<body>
+    <div class="container">
+
+		<div class="span10 offset1">
+		
+			<div class="row">
+				
+			</div>
+
+			<div class="row">
+				<h3>Login</h3>
+			</div>
+
+			<form class="form-horizontal" action="login.php" method="post">
+								  
+				<div class="control-group">
+					<label class="control-label">Username (Email)</label>
+					<div class="controls">
+						<input name="username" type="text"  placeholder="me@email.com" required> 
+					</div>	
+				</div> 
+				
+				<div class="control-group">
+					<label class="control-label">Password</label>
+					<div class="controls">
+						<input name="password" type="password"> 
+					</div>	
+				</div> 
+
+				<div class="form-actions">
+					<button type="submit" class="btn btn-success">Sign in</button>
+					&nbsp; &nbsp;
+					<a class="btn btn-primary" href="fr_per_create2.php">Get started today!</a>
+				</div>
+				
+			
+				
+				<br />
+			
+
+				
+			</form>
+
+
+		</div> <!-- end div: class="span10 offset1" -->
+				
+    </div> <!-- end div: class="container" -->
+
+  </body>
+  
+</html>
